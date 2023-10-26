7ad98b40cf9f66a357a001e698ce679cba493f52
aqueelahi <aqueelahirshad2@gmail.com>
2023-10-26 18:17:37 -0400

    UPLOADING CODE

---
+++ Semester1.py
@@ -0,0 +1,36 @@
+
+tryAgain = True
+
+while tryAgain:
+    
+    name = input ("Welcome! What is your name?: ")
+    #Asking the user's name
+
+    temperature = input ("What is the temperature today?: ")
+
+    mood = input ("What is your mood today?: ")
+    #Asking the user's mood
+
+    if mood == "good":
+        print( "Great, you're feeling good")
+    elif mood == "bad":
+        print( "I'm sorry you're feeling like this")
+    else: 
+        print("It looks you're feeling neutral")
+
+    restart = input ("Try again? Enter y for yes, n for no: ")
+    y == True
+    n == False
+
+
+    if tryAgain == False: 
+        print ("You look great!")
+        # Asking the user if they want to repeat the process
+
+
+
+
+
+# if answer == 'yes':
+# if answer != 'yes'
+    
